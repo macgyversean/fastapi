04e6f3ae0be5c773781ec664b816f28ed4a863a5
Sean Carroll <macgyversean@gmail.com>
2024-02-13 14:56:10 -0500

    works with item/1 in url

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/items/")
-async def root():
-    return {"message": "Hello World"}
+# Create our root route
+@app.get("/items/{item_id}")
+def root():
+    return {"message": "Hello, World!"}
+def read_item(item_id: int):
+    return {"item_id": item_id}
