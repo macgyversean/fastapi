137b1fd78157b42ddb3ded16f27e12388630f9c9
Sean Carroll <macgyversean@gmail.com>
2024-02-13 16:08:41 -0500

    added users and user id

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Create our root route
-@app.get("/items/{item_id}")
-def root():
+@app.get("/")
+def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/items/")
+
+def read_items(skip: int = 0, limit: int = 10, q: str = None):
+    if q:
+        return {"skip": skip, "limit": limit, "q": q}
+    else:
+        return {"skip": skip, "limit": limit}
+    
+@app.get("users/{user_id}")
+def read_user_items(user_id):
+    return {"user_id": user_id}
